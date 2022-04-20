f01a46c56c106164ca6b23b6d57416c576938f31
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-04-20 19:54:09 +0900

    Created Gulp Files
    Finished Pug, Webserver, watching files, Image Optimization

---
+++ gulpfile.babel.js
@@ -0,0 +1,44 @@
+import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
+
+const routes = {
+  pug: {
+    watch: "src/**/*.pug",
+    src: "src/*.pug",
+    dest: "build",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
+};
+
+const pug = () =>
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+const clean = () => del(["build"]);
+
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const img = async () => {
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+};
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.series([pug]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);

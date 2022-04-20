b44bbc1bf0aa44e42961a0f1ca4ba740fdb706d6
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-04-20 22:47:22 +0900

    Created Deploy to Gh Pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,7 @@ import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 const sass = gulpSass(dartSass);
 
@@ -37,25 +38,23 @@ const routes = {
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build", ".publish"]);
 
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-const img = async () => {
+const img = () =>
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
-};
 
-const styles = async () => {
+const styles = () =>
   gulp
     .src(routes.scss.src)
     .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer())
     .pipe(miniCSS())
     .pipe(gulp.dest(routes.scss.dest));
-};
 
-const js = async () => {
+const js = () =>
   gulp
     .src(routes.js.src)
     .pipe(
@@ -67,7 +66,8 @@ const js = async () => {
       })
     )
     .pipe(gulp.dest(routes.js.dest));
-};
+
+const gh = () => gulp.src("build/**/*").pipe(ghPages());
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
@@ -79,6 +79,8 @@ const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug, styles, js]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean]);

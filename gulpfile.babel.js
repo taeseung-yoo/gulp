3fbc10f7b1afb414de06c3e9c56839df0a5a7238
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-04-20 21:55:03 +0900

    Created Scss & JS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,9 +3,14 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
-import sass from "gulp-sass";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
 
-sass.compiler = require("node-sass");
+const sass = gulpSass(dartSass);
 
 const routes = {
   pug: {
@@ -16,6 +21,16 @@ const routes = {
   img: {
     src: "src/img/*",
     dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js",
   },
 };
 
@@ -31,13 +46,38 @@ const img = async () => {
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 };
 
+const styles = async () => {
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+};
+
+const js = async () => {
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
+};
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles, js]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
